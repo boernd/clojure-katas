72d718bb6a77857d50bb4e4b263aa0ac0821652f
bhois <bernd.hois@bearingpoint.com>
2018-10-09 20:56:05 +0200

    4clojure26

---
+++ src/fclojure/4clojure26.clj
@@ -0,0 +1,25 @@
+(ns fclojure.4clojure26)
+
+(defn samplefn
+  [__]
+  (and
+    (= (__ 3) '(1 1 2))
+    (= (__ 6) '(1 1 2 3 5 8))
+    (= (__ 8) '(1 1 2 3 5 8 13 21))))
+
+(defn testfn
+  [__]
+  [
+    (= (__ 3) '(1 1 2))
+    (= (__ 6) '(1 1 2 3 5 8))
+    (= (__ 8) '(1 1 2 3 5 8 13 21))])
+
+(defn solve26
+  [len]
+  (loop [x len
+         f 0
+         s 1
+         result []]
+    (if (= x 0)
+      result
+      (recur (- x 1) s (+ f s) (conj result s)))))
